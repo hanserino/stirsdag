7789ce50241da9b3780feb2f0705176de43910f8
Hans Kristian Smedsrød <eplehans@gmail.com>
2018-05-28 21:28:14 +0200

    first stabil version

--- er-det-stirsdag/modules/isit.js
+++
@@ -1,9 +0,0 @@
-const isit = function(daysUntil, today){
-
-    //Stirsdag is Tuesday, aka 2nd day of the week
-    let isStirsdag = today === 2 ? true : false;
-
-    return isStirsdag;
-}
-
-module.exports = isit
